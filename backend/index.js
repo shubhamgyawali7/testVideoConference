d35defa7cae27f51b1226313e3831cf8e4c227bf
Shubham Gyawali <shubhamgyawali11@gmail.com>
2025-09-26 14:50:30 +0545

    updated

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,10 @@ const server = http.createServer(app);
 app.use(morgan("combined"));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel!");
+});
+
 // Socket.IO setup
 const io = new SocketIo(server, {
   cors: {
